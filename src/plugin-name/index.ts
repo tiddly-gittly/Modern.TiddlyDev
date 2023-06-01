4ba79ae75b013013e3035b579d0adfd9514b7107
WhiteFall <1947114574@qq.com>
2023-06-01 19:50:39 +0800

    fix #37 #39 #36 #34

--- src/plugin-name/index.ts
+++ src/plugin-name/index.ts
@@ -17,4 +17,9 @@ class ExampleWidget extends Widget {
   }
 }
 
-exports.test = ExampleWidget;
+// 此处导出的模块名RandomNumber将作为<$RandomNumber/>：widget的名称。
+// 另外，index.ts.meta元信息文件中的条目名字名称可以与导出的模块名不一致。
+// 可以没有元信息文件。但index.ts文件与index.ts.meta元信息文件的文件名必须一致。
+// 例如：index.ts文件与index.ts.meta文件，一一对应。
+// 如果为一个脚本文件添加了 .meta 将会被视为入口文件。
+exports.RandomNumber = ExampleWidget;
