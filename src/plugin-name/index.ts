247930e366eac7eeaf4d9caa929be5db5bad3b2e
WhiteFall <1947114574@qq.com>
2023-06-02 10:19:32 +0800

    更新微件名与条目名与源文件名可以不一致。

--- src/plugin-name/index.ts
+++ src/plugin-name/index.ts
@@ -17,9 +17,8 @@ class ExampleWidget extends Widget {
   }
 }
 
-// 此处导出的模块名RandomNumber将作为<$RandomNumber/>：widget的名称。
-// 另外，index.ts.meta元信息文件中的条目名字名称可以与导出的模块名不一致。
-// 可以没有元信息文件。但index.ts文件与index.ts.meta元信息文件的文件名必须一致。
-// 例如：index.ts文件与index.ts.meta文件，一一对应。
+// 此处导出的模块变量名RandomNumber将作为微件（widget）的名称。使用<$RandomNumber/>调用此微件。
+// Widget在tiddlywiki中的条目名、源文件以及源文件.meta文件名和Widget名字可以不一致。
+// 比如Widget条目名可以为My-Widget,源文件以及源文件.meta文件名可以称为index.ts与index.ts.meta。最终的Widget名却是：RandomNumber，且使用<$RandomNumber/>调用此微件。
 // 如果为一个脚本文件添加了 .meta 将会被视为入口文件。
 exports.RandomNumber = ExampleWidget;
