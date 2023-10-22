7fbe750575565b26c771b897bfc690b9539296bb
oeyoews <jyao4783@gmail.com>
2023-10-22 14:52:05 +0800

    Update tailwind.config.js
    disable prelight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{mjs,cjs,js,ts,jsx,tsx}'],
+  // important: true,
+  corePlugins: {
+    preflight: false,
+  },
   theme: { extend: {} },
   plugins: [],
 };
